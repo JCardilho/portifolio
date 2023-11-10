176a5daf4e511e81b21f5cbabf33428c1674edc9
JCardilho <jose.1433@hotmail.com>
2023-11-09 22:33:11 -0300

    corrigindo codigo responsividade

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function openNav(){
     var x = document.getElementById("navigation");
     if (x.className==="navigation"){
         x.className += "menujs";
-        document.getElementById("threeeline-icon").innerHTML="&Cross";
+        document.getElementById("threeline-icon").innerHTML="&Cross";
     }else {
         x.className ="navigation"
-        document.getElementById("threeeline-icon").innerHTML="&#9776<";
+        document.getElementById("threeline-icon").innerHTML="&#9776<";
     }
 }
